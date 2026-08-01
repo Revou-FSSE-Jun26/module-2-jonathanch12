0b78990d1972c43fd529b3162f887b512e01ea1c
jonathanch12 <164126841+jonathanch12@users.noreply.github.com>
2026-08-01 12:59:09 +0700

    Add initial database schema, seed data, queries, and ERD

---
+++ queries.sql
@@ -0,0 +1,12 @@
+--Query to retrieve list of products with the stock being more than 20
+select product_id, product_name, price, stock
+from products
+where stock > 20
+order by price desc
+limit 5;
+
+--Query to retrieve list of users whose name contains the letter 'A'
+select name, email
+from users
+where name like '%A%'
+order by name;
---
+++ seed.sql
@@ -0,0 +1,46 @@
+insert into users(name, email, phone, address)
+values
+('John Alexander', 'john@email.com', '081234567890', 'Park Ave 123'),
+('Sarah Tan', 'sarah@email.com', '081345678901', 'Milton 1'),
+('Michael Max', 'michael@email.com', '081456789012', 'Sora Area'),
+('Alonso Wirtz', 'alonso@email.com', '081567890123', 'Keynes 123'),
+('David Alten', 'david@email.com', '081678901234', 'Stamford 4');
+
+insert into categories(category_name, description)
+values
+('Electonics', 'Electronic gadgets'),
+('Books', 'Educational and fiction books'),
+('Home', 'Home appliances'),
+('Sports', 'Sporting equipment'),
+('Fashion', 'Clothing and accessories');
+
+insert into products(category_id, product_name, description, price, stock)
+values
+(1,'Logitech Wireless Mouse','Bluetooth Mouse',199000,50),
+(1,'Razer Mechanical Keyboard','RGB Keyboard',799000,25),
+(2,'Learning PostgreSQL','Database Book',350000,40),
+(2,'Refactoring: Clean Code','Programming Book',450000,30),
+(3,'Rice Cooker','1.8 Liter Rice Cooker',650000,15),
+(3,'Electric Kettle','1.5 Liter',250000,35),
+(4,'Spain RFEF Men Jersey','Official Size M',180000,60),
+(4,'Jabulani Ball','Football Equipment',275000,45),
+(5,'Hoodie','Cotton Hoodie',320000,20),
+(5,'Sneakers','Running Shoes',850000,18);
+
+insert into orders(user_id, total_amount, status)
+values
+(1,998000,'Completed'),
+(2,350000,'In Process'),
+(3,1370000,'Completed'),
+(1,275000,'Completed'),
+(5,850000,'In Process');
+
+insert into order_items(order_id, product_id, quantity, unit_price)
+values
+(1,1,1,199000),
+(1,2,1,799000),
+(2,3,1,350000),
+(3,4,2,450000),
+(3,7,2,180000),
+(4,8,1,275000),
+(5,10,1,850000);
